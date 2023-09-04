c1a83a7a0393ec77e10052a1f06c69b23971a470
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2023-09-04 01:42:47 -0700

    fixed about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -45,13 +45,13 @@ export default function About() {
         <p>
           I am actively seeking job and experience opportunities to leverage and
           develop my skills and knowledge. I plan on garnering some internship and
-          industry experience throughout the course of my undergrad studie. Upon completion
-          of my undergraduate studies, I hope to land a full-time job and develop a career
+          industry experience throughout the course of my undergrad studies. Upon completion
+          of my undergraduate degree, I hope to land a full-time job and develop a career
           in these industries.
         </p>
         <p>
           I am currently enrolled in the UC Berkeley Coding Bootcamp where I have honed my
-          abilities and developed various projects that are attached to my portfolio below.
+          abilities and developed various projects that are attached to my portfolio.
           With a passion in finance and technology, I am poised to contribute to the ever-changing
           of the tech and financial tech industries.
         </p>
